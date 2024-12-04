fe0843b2675f80932ea2e791b35e35a07238ffec
iperdids <ipersids@student.hive.fi>
2024-12-04 19:26:44 +0200

    Update Makefile to build submodule ft_printf

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: ipersids <ipersids@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 11:52:27 by ipersids          #+#    #+#             */
-/*   Updated: 2024/12/04 15:42:19 by ipersids         ###   ########.fr       */
+/*   Updated: 2024/12/04 19:24:55 by ipersids         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@
 # include <string.h>		// strerror
 // # include <math.h>		// math library
 
+# include "ft_printf.h"		// ft_printf library
+
 
 # ifndef WIDTH
 #  define WIDTH 1366
@@ -33,7 +35,5 @@
 # define NAME "So loooooooooooong game!"
 
 void	input_key_hook(mlx_key_data_t keydata, void* param);
-size_t	ft_putstrnl_fd(char *s, int fd);
-size_t	ft_putunbr_fd(unsigned int n, int fd);
 
 #endif
